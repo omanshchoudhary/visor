ee72520a6e60ae02bc84d84554669687a0d52006
Omansh Choudhary <omansh.choudhary@gmail.com>
2026-01-11 05:57:08 +0530

    Calculate revenue and signup totals from daily stats

--- js/data.js
+++ js/data.js
@@ -1,7 +1,18 @@
-async function fetchData() {
+async function localDashboard() {
+    let totalRevenue=0
+    let totalSignups=0
+    let totalConversion=0;
+
     const response = await fetch('data/data.json')
     const data = await response.json()
-    console.log(data)    
+    console.log(data) 
+    data.dailyStats.forEach(stat => {
+        totalRevenue+=stat.revenue;
+        totalSignups+=stat.signups
+        totalConversion += stat.conversionRate
+    }); 
+    totalConversion/=data.dailyStats.length 
+    console.log(totalConversion) 
 }
 
-fetchData()
+localDashboard()
